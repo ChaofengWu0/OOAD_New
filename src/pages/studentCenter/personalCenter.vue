921126333362e27f3863134ecbba352c391532a2
ChaofengWu0 <1720751884@qq.com>
2022-11-24 18:02:07 +0800

    修改了学生的个人中心的样式

--- src/pages/studentCenter/personalCenter.vue
+++ src/pages/studentCenter/personalCenter.vue
@@ -4,7 +4,7 @@
     <div class="info_box">
       <div class="left_ele">
         <div class="avatar">
-          <el-avatar :size="250" :src="this.avatar_path"></el-avatar>
+          <el-avatar :size="250" :src="this.original_data.avatar_path"></el-avatar>
         </div>
 
       </div>
@@ -12,16 +12,44 @@
 
       <div class="right_ele">
         <div class="right_container">
-          <el-descriptions title="Info" class="info_items" :column="1">
-            <el-descriptions-item label="UserId">{{ this.id }}</el-descriptions-item>
-            <el-descriptions-item label="Name">{{ this.name }}</el-descriptions-item>
-            <el-descriptions-item label="Phone">{{ this.phone }}</el-descriptions-item>
-            <el-descriptions-item label="Address">{{ this.address }}</el-descriptions-item>
-            <el-descriptions-item label="Subject">{{ this.subject }}</el-descriptions-item>
-          </el-descriptions>
+          <ul class="info_items">
+            <li style="list-style:none">
+              <h3> 账号: {{ this.original_data.id }} </h3>
+            </li>
+            <li style="list-style:none">
+              <h3> 名称: {{ this.original_data.name }}</h3>
+            </li>
+            <li style="list-style:none">
+              <h3> 邮件: {{ this.original_data.email }}</h3>
+            </li>
+            <li style="list-style:none">
+              <h3> 电话: {{ this.original_data.phone }}</h3>
+            </li>
+          </ul>
+          <el-button type="text" class="down_ele" @click="edit_info" style="color: black">
+            <h3 style="color: #DD4A68">编 辑</h3>
+          </el-button>
+
         </div>
       </div>
     </div>
+
+
+    <el-dialog title="个人信息" :visible.sync="dialogFormVisible">
+      <el-form :model="change_form">
+        <el-form-item label="" :label-width="formLabelWidth">
+          <el-input v-model="change_form.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="cancel" style="width: 80px; height: 50px">
+          <h3>取 消</h3>
+        </el-button>
+        <el-button type="primary" @click="submit" style="width: 80px; height: 50px">
+          <h3>确 定</h3>
+        </el-button>
+      </div>
+    </el-dialog>
 
   </div>
 </template>
@@ -31,12 +59,29 @@ export default {
   name: "personalCenter",
   data() {
     return {
-      id: this.$store.state.id,
-      name: this.$store.state.name,
-      avatar_path: this.$store.state.avatar_path,
-      phone: this.$store.state.phone,
-      address: this.$store.state.address,
-      subject: this.$store.state.subject
+      original_data: {
+        id: this.$store.state.id,
+        name: this.$store.state.name,
+        avatar_path: this.$store.state.avatar_path,
+        phone: this.$store.state.phone,
+        address: this.$store.state.address,
+        subject: this.$store.state.subject,
+      },
+
+      formLabelWidth: "120px",
+      dialogFormVisible: false,
+      change_form: {}
+    }
+  },
+  methods: {
+    edit_info() {
+      this.dialogFormVisible = true
+    },
+    cancel() {
+      this.dialogFormVisible = false
+    },
+    submit() {
+      this.dialogFormVisible = false
     }
   },
 
@@ -68,14 +113,21 @@ export default {
   height: 100%;
   width: 70%;
   right: 0;
-  background-color: #42b983;
+  background-image: url("../../assets/img/personalCenter.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100% 100%;
+  /*background-color: #42b983;*/
 }
 
 .left_ele {
   position: absolute;
+  background-image: url("../../assets/img/personalCenterLeft.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100% 100%;
   height: 100%;
   width: 30%;
-  background-color: #795da3;
 }
 
 .avatar {
@@ -84,16 +136,24 @@ export default {
   left: 18%;
 }
 
-.right_container{
+.right_container {
   position: absolute;
   top: 30%;
+  width: 100%;
+
 
 }
 
-.info_items{
+.info_items {
+  background-color: white;
   height: 100%;
   width: 100%;
   /*position: absolute;*/
+}
+
+li {
+  font-family: 隶书;
+  font-size: 20px;
 }
 
 
