899a823870cfcaf685dc2a7ca1d322891f13bebd
Lynchrocket <lynchrocket@gmail.com>
2022-12-01 23:06:38 +0800

    Update courseDetail.vue

--- src/pages/teacherCenter/classShow/courseDetail.vue
+++ src/pages/teacherCenter/classShow/courseDetail.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-
     <template>
       <el-table
           ref="multipleTable"
@@ -10,46 +9,20 @@
           class="list_content">
         <el-table-column>
           <el-button @click="previous" type="success">返回课程列表</el-button>
-          <el-button @click.native.prevent="getStudent" type="primary">课程学生名单</el-button>
-          <el-button @click.native.prevent="get_chapter" type="primary">查看课程章节</el-button>
-          <el-button @click.native.prevent="send_notice" @click="dialogFormVisible=true" type="primary">
+          <el-button @click.native.prevent="toStudentList" type="primary">课程学生名单</el-button>
+          <el-button @click.native.prevent="toChapters" type="primary">查看课程章节</el-button>
+          <el-button @click="dialogFormVisible=true" type="primary">
             发送通知
           </el-button>
-
           <el-button @click="email_dialog=true" type="primary">
             发送邮件
           </el-button>
         </el-table-column>
       </el-table>
-
-      <template>
-        <el-dialog title="发送内容" :visible.sync="email_dialog">
-          <el-input v-model="email_return.subject" style="margin-bottom: 20px" placeholder="请输入标题">
-          </el-input>
-          <el-input
-              placeholder="请输入内容"
-              type="textarea"
-              :row="10"
-              v-model="email_return.text"
-              size="large"
-          >
-          </el-input>
-
-          <div slot="footer" class="dialog-footer">
-            <el-button @click="cancel_email">取 消</el-button>
-            <el-button type="primary" @click="submit_email">确 定</el-button>
-          </div>
-        </el-dialog>
-      </template>
-
-
       <template>
         <el-dialog title="发送内容" :visible.sync="dialogFormVisible">
-
           <el-input v-model="notice_title" style="margin-bottom: 20px" placeholder="请输入标题">
-
           </el-input>
-
           <el-input
               placeholder="请输入内容"
               type="textarea"
@@ -58,40 +31,44 @@
               size="large"
           >
           </el-input>
-
           <div slot="footer" class="dialog-footer">
-            <el-button @click="cancel">取 消</el-button>
-            <el-button type="primary" @click="submit">确 定</el-button>
+            <el-button @click="cancelNotice">取 消</el-button>
+            <el-button type="primary" @click="sendNotice">确 定</el-button>
           </div>
         </el-dialog>
       </template>
-
     </template>
-
+    <template>
+      <el-dialog title="发送内容" :visible.sync="email_dialog">
+        <el-input v-model="email_return.sendTo" style="margin-bottom: 20px"
+                  placeholder="请输入收件人（为空时发送给课程下的所有学生）"></el-input>
+        <el-input v-model="email_return.subject" style="margin-bottom: 20px" placeholder="请输入标题"></el-input>
+        <el-input
+            placeholder="请输入内容"
+            type="textarea"
+            :row="10"
+            v-model="email_return.text"
+            size="large"
+        ></el-input>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="cancelEmail">取 消</el-button>
+          <el-button type="primary" @click="sendEmail">确 定</el-button>
+        </div>
+      </el-dialog>
+    </template>
     <div class="class_info">
-
       <img :src="course_cover" class="course_img"/>
-
       <div class="text_info">
         课程详情介绍:
         <br>
         {{ course_text_info }}
       </div>
-
-
     </div>
-
-
   </div>
 </template>
 
 <script>
-// todo
-// 1. 在created()中要利用courseID获取上这门课的所有人
-// 2. 在export_grade()中要导出这一个学生的成绩，怎么导出来呢？不知道
-// 3. 在delete_student()中要删除这个学生，给后端发送 course_id，以及这个学生的id，后端负责改变数据库内容
 import requestUtil from "@/utils/request";
-
 
 export default {
   name: "courseDetail",
@@ -101,23 +78,19 @@ export default {
       course_text_info: null,
       course_id: null,
       notice_title: null,
-
       notice_return: {
         title: this.notice_title,
         courseId: this.course_id,
         content: this.notice,
         teacherId: null,
       },
-
       email_return: {
-        filePath: "/",
-        subject: this.notice_title,
-        sendTo: this.course_id,
-        text: this.notice
+        filePath: null,
+        subject: null,
+        sendTo: null,
+        text: null
       },
-
       email_dialog: false,
-
       notice: "",
       dialogFormVisible: false,
       student_list: [{
@@ -129,98 +102,22 @@ export default {
       }],
     }
   },
-
   created() {
     this.getCourseID()
     this.getUserList()
     this.notice_return.teacherId = JSON.parse(sessionStorage.getItem("userInfo")).data.id
   },
-
   methods: {
-    cancel_email() {
-      this.email_return = {
-        filePath: "/",
-        subject: this.notice_title,
-        sendTo: this.course_id,
-        text: this.notice
-      }
-    },
-
-    async submit_email() {
-      this.email_return.sendTo = '1720751884@qq.com'
-      this.email_return.filePath = null
-      console.log(111111111111111111111111)
-      console.log(this.email_return)
-      const {data: res} = await requestUtil.post('http://localhost:8002/eduoss/send-mail/simple', this.email_return)
-      console.log(res)
-      if (res.code === 20000) {
-        this.$message.success("邮件发送成功")
-      }
-      this.email_return = {
-        filePath: "/",
-        subject: null,
-        sendTo: null,
-        text: null
-      }
-    },
-
-    previous() {
-      this.$router.push('/teacher_center/my_classes/class_list')
-    },
-
-    get_chapter() {
-      this.$router.push({path: '/teacher_center/my_classes/view_chapter/' + this.course_id})
-    },
-
-    async getUserList() {
-      // todo
-      const {data: res} = await requestUtil.get('/eduservice/edu-course/getCourseDetailById/' + this.course_id)
-      console.log(res);
-      this.course_text_info = res.data.description
-      this.course_cover = res.data.cover
-      if (res.code !== 20000)
-        return this.$message.error("Wrong! Renderer failed")
-    },
-
-    getCourseID() {
-      if (this.$route.params && this.$route.params.id) {
-        this.course_id = this.$route.params.id
-
-        console.log(this.course_id)
-        console.log(this.notice_return.courseId)
-      } else {
-        this.$message("Wrong in function getCourseID which is in classChapter.Vue ")
-      }
-    },
-
-    getStudent() {
-      this.$router.push({path: '/teacher_center/my_classes/student_list/' + this.course_id})
-    },
-
-    // async send_email() {
-    //   const {data: res} = await requestUtil.post('/send-email/simple', this.email_return)
-    //   console.log(res);
-    //   if (res.data.code === 20000) {
-    //     this.$message.success("邮件发送成功")
-    //   }
-    // },
-
-    send_notice() {
-      console.log(this.notice_return.teacherId)
-    },
-
-    cancel() {
+    cancelNotice() {
       this.dialogFormVisible = false
       this.notice = ""
       this.notice_title = ""
     },
-
-    async submit() {
+    async sendNotice() {
       this.notice_return.title = this.notice_title
       this.notice_return.courseId = this.course_id
       this.notice_return.content = this.notice
       const {data: res} = await requestUtil.post('/eduservice/t-notice', this.notice_return)
-      console.log(res)
       if (res.code === 20000) {
         this.$message.success("通知发送成功")
       }
@@ -228,9 +125,57 @@ export default {
       this.notice = ""
       this.notice_title = ""
     },
-
+    cancelEmail() {
+      this.email_return = {
+        filePath: null,
+        subject: null,
+        sendTo: null,
+        text: null
+      }
+    },
+    async sendEmail() {
+      let sendMailUrl = 'http://localhost:8001/eduservice/send-mail/simple'
+      if (this.email_return.sendTo === null) {
+        sendMailUrl = 'http://localhost:8001/eduservice/send-mail/simple/all/' + this.course_id
+      }
+      const {data: res} = await requestUtil.post(sendMailUrl, this.email_return)
+      if (res.code === 20000) {
+        this.email_dialog = false
+        this.email_return = {
+          filePath: null,
+          subject: null,
+          sendTo: null,
+          text: null
+        }
+        this.$message.success("邮件发送成功")
+      } else {
+        this.$message.success("邮件发送失败请重试")
+      }
+    },
+    async getUserList() {
+      const {data: res} = await requestUtil.get('/eduservice/edu-course/getCourseDetailById/' + this.course_id)
+      this.course_text_info = res.data.description
+      this.course_cover = res.data.cover
+      if (res.code !== 20000)
+        return this.$message.error("Wrong! Renderer failed")
+    },
+    getCourseID() {
+      if (this.$route.params && this.$route.params.id) {
+        this.course_id = this.$route.params.id
+      } else {
+        this.$message("Wrong in function getCourseID which is in classChapter.Vue ")
+      }
+    },
+    previous() {
+      this.$router.push('/teacher_center/my_classes/class_list')
+    },
+    toChapters() {
+      this.$router.push({path: '/teacher_center/my_classes/view_chapter/' + this.course_id})
+    },
+    toStudentList() {
+      this.$router.push({path: '/teacher_center/my_classes/student_list/' + this.course_id})
+    },
   },
-
 }
 </script>
 
